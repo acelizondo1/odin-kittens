8e21d9cea8b6a00397c4a3f3c1f8166085896ece
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-30 09:46:34 -0500

    adding basic flash messages and erro handling for form

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -19,8 +19,10 @@ class KittensController < ApplicationController
     def create 
         @kitten = Kitten.new(kitten_params)
         if @kitten.save
+            flash[:notice] = "Kitten Info Was Saved"
             redirect_to @kitten
         else
+            flash[:error] = "Kitten Info Was Not Created"
             render :new
         end
     end
@@ -28,8 +30,10 @@ class KittensController < ApplicationController
     def update 
         @kitten = Kitten.find(params[:id])
         if @kitten.update(kitten_params)
+            flash[:notice] = "Kitten Updated"
             redirect_to @kitten
         else
+            flash[:error] = "Issue Updating Kitten Info"
             redirect_to edit_kitten_path(@kitten)
         end
     end
@@ -37,8 +41,10 @@ class KittensController < ApplicationController
     def destroy
         @kitten = Kitten.find(params[:id])
         if @kitten.destroy
+            flash[:notice] = "Kitten Info Deleted"
             redirect_to kittens_path
         else
+            flash[:error] = "Issue Deleting Kitten Info"
             redirect_to @kitten
         end
     end
