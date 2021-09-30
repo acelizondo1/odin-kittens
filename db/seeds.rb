b60cb0f8d04fb795812cf7eff8966d284c1edfdb
Aaron Elizondo <acelizondo11@gmail.com>
2021-09-29 22:13:01 -0500

    Creating index page and seeding initial kitten data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+NAMES = ["Rose","Tulip","Pansy","Lily","Lavender","Dandelion","Daisy","Iris","Buttercup","Violet","Poppy","Camellia","Petunia","Aster","Dahlia",
+    "Jasmine","Flower","Fern","Holly","Jude","Maple","Ash","Mulberry","Magnolia","Basil","Rosemary","Sage","Peppermint","Thyme","Lavender","Ivy",
+    "Willow","Blueberry","Pumpkin","Kiwi","Mango","Apple","Okra","Peaches","Clementine","Cherry","Pepper","Tomato","Celery ","Cinnamon","Pepper",
+    "Nutmeg","Ginger","Paprika","Hazel","Sesame","Cardamom", "Nugget","Nacho","Cookie","Lucky","Oreo","Snickers","Sugar","Twix","Cocoa","Gelato",
+    "Soda Pop","Fluffy","Padfoot","Sirius","Dobby","Norbert","Pixie","Black","Hermione","Dumbledore","Fleur","Luna","Weasley","Malfoy","Ginny",
+    "Hedwig"]
+
+NAMES.each do |name|
+    Kitten.create({
+        :name=>name, 
+        :age=>rand(1..20),
+        :cuteness=>rand(0..10),
+        :softness=>rand(0..10)
+    })
+end
